aedcf6ca9f1d78f69a1ed7eff351005031c69749
Rocky <rockygitt@gmail.com>
2020-12-14 21:05:49 +0100

    AoC Problem 02

---
+++ 02/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type requirement struct {
+	minimum   int
+	maximum   int
+	character byte
+	password  string
+}
+
+func main() {
+	file, _ := os.Open("./input.txt")
+
+	defer file.Close()
+
+	var reqs = []requirement{}
+
+	scanner := bufio.NewScanner(file)
+
+	reMinimum := regexp.MustCompile(`^\d+`)
+	reMaximum := regexp.MustCompile(`\d+\s`)
+	reCharacter := regexp.MustCompile(`\D:`)
+	rePassword := regexp.MustCompile(`:\s\w+`)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		minimum, _ := strconv.Atoi(reMinimum.FindString(line))
+		maximum, _ := strconv.Atoi(strings.Trim(reMaximum.FindString(line), " "))
+		character := reCharacter.FindString(line)[0]
+		password := rePassword.FindString(line)[2:]
+
+		reqs = append(reqs, requirement{minimum, maximum, character, password})
+	}
+
+	numValid := 0
+
+	for _, r := range reqs {
+		if r.minimum <= strings.Count(r.password, string(r.character)) {
+			if strings.Count(r.password, string(r.character)) <= r.maximum {
+				numValid++
+			}
+		}
+	}
+
+	fmt.Printf("Number of valid passwords (1): %d\n", numValid)
+
+	numValid = 0
+
+	for _, r := range reqs {
+		if (r.password[r.minimum-1] == r.character) != (r.password[r.maximum-1] == r.character) {
+			numValid++
+		}
+	}
+
+	fmt.Printf("Number of valid passwords (2): %d\n", numValid)
+}
