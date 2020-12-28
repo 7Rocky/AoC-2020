3617a4a030027c262d35c9dce417e074368c25f9
Rocky <rockygitt@gmail.com>
2020-12-28 18:17:14 +0100

    AoC Problem 04

--- 04/main.go
+++ 04/main.go
@@ -8,50 +8,42 @@ import (
 	"strings"
 )
 
-func isValidPassport(passport []byte, level int) bool {
+func isValidPassport(passport string, level int) bool {
 	var (
-		patternbyr string
-		patterniyr string
-		patterneyr string
-		patternhgt string
-		patternhcl string
-		patternecl string
-		patternpid string
+		rebyr *regexp.Regexp
+		reiyr *regexp.Regexp
+		reeyr *regexp.Regexp
+		rehgt *regexp.Regexp
+		rehcl *regexp.Regexp
+		reecl *regexp.Regexp
+		repid *regexp.Regexp
 	)
 
 	if level == 1 {
-		patternbyr = `byr:`
-		patterniyr = `iyr:`
-		patterneyr = `eyr:`
-		patternhgt = `hgt:`
-		patternhcl = `hcl:`
-		patternecl = `ecl:`
-		patternpid = `pid:`
+		rebyr = regexp.MustCompile("byr:")
+		reiyr = regexp.MustCompile("iyr:")
+		reeyr = regexp.MustCompile("eyr:")
+		rehgt = regexp.MustCompile("hgt:")
+		rehcl = regexp.MustCompile("hcl:")
+		reecl = regexp.MustCompile("ecl:")
+		repid = regexp.MustCompile("pid:")
 	} else if level == 2 {
-		patternbyr = `(byr:19[2-9][0-9])|(byr:200[0-2])`
-		patterniyr = `(iyr:201[0-9])|(iyr:2020)`
-		patterneyr = `(eyr:202[0-9])|(eyr:2030)`
-		patternhgt = `(hgt:1[5-8][0-9]cm)|(hgt:19[0-3]cm)|(hgt:59in)|(hgt:6[0-9]in)|(hgt:7[0-6]in)`
-		patternhcl = `(hcl:#[0-9a-f]{6})`
-		patternecl = `(ecl:amb)|(ecl:blu)|(ecl:brn)|(ecl:gry)|(ecl:grn)|(ecl:hzl)|(ecl:oth)`
-		patternpid = `pid:[0-9]{9}`
+		rebyr = regexp.MustCompile("(byr:19[2-9][0-9])|(byr:200[0-2])")
+		reiyr = regexp.MustCompile("(iyr:201[0-9])|(iyr:2020)")
+		reeyr = regexp.MustCompile("(eyr:202[0-9])|(eyr:2030)")
+		rehgt = regexp.MustCompile("(hgt:1[5-8][0-9]cm)|(hgt:19[0-3]cm)|(hgt:59in)|(hgt:6[0-9]in)|(hgt:7[0-6]in)")
+		rehcl = regexp.MustCompile("(hcl:#[0-9a-f]{6})")
+		reecl = regexp.MustCompile("(ecl:amb)|(ecl:blu)|(ecl:brn)|(ecl:gry)|(ecl:grn)|(ecl:hzl)|(ecl:oth)")
+		repid = regexp.MustCompile("pid:[0-9]{9}")
 	}
 
-	rebyr := regexp.MustCompile(patternbyr)
-	reiyr := regexp.MustCompile(patterniyr)
-	reeyr := regexp.MustCompile(patterneyr)
-	rehgt := regexp.MustCompile(patternhgt)
-	rehcl := regexp.MustCompile(patternhcl)
-	reecl := regexp.MustCompile(patternecl)
-	repid := regexp.MustCompile(patternpid)
-
-	return rebyr.Match(passport) &&
-		reiyr.Match(passport) &&
-		reeyr.Match(passport) &&
-		rehgt.Match(passport) &&
-		rehcl.Match(passport) &&
-		reecl.Match(passport) &&
-		repid.Match(passport)
+	return rebyr.MatchString(passport) &&
+		reiyr.MatchString(passport) &&
+		reeyr.MatchString(passport) &&
+		rehgt.MatchString(passport) &&
+		rehcl.MatchString(passport) &&
+		reecl.MatchString(passport) &&
+		repid.MatchString(passport)
 }
 
 func main() {
@@ -75,18 +67,15 @@ func main() {
 		}
 	}
 
-	numValid := []int{0, 0}
+	for _, level := range []int{1, 2} {
+		numValid := 0
 
-	for _, p := range passports {
-		if isValidPassport([]byte(strings.Trim(p, " ")), 1) {
-			numValid[0]++
+		for _, p := range passports {
+			if isValidPassport(strings.Trim(p, " "), level) {
+				numValid++
+			}
 		}
 
-		if isValidPassport([]byte(strings.Trim(p, " ")), 2) {
-			numValid[1]++
-		}
+		fmt.Printf("Valid passports (%d): %d\n", level, numValid)
 	}
-
-	fmt.Printf("Valid passports (1): %d\n", numValid[0])
-	fmt.Printf("Valid passports (2): %d\n", numValid[1])
 }
