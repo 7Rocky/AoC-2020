f8f833db53106f4871b1fbb9bf5240aac4d571ca
Rocky <rockygitt@gmail.com>
2021-02-10 20:20:39 +0100

    AoC Problem 25

--- day_25/main.go
+++ day_25/main.go
@@ -7,14 +7,17 @@ import (
 	"strconv"
 )
 
+func productMod(a, b, m int) int {
+	return (a * b) % m
+}
+
 func getLoopSize(subjectNumber, divisor, publicKey int) int {
 	num := 1
 	loopSize := 0
 
 	for num != publicKey {
 		loopSize++
-		num *= subjectNumber
-		num %= divisor
+		num = productMod(num, subjectNumber, divisor)
 	}
 
 	return loopSize
@@ -24,8 +27,7 @@ func getEncryptionKey(subjectNumber, divisor, loopSize int) int {
 	num := 1
 
 	for i := 0; i < loopSize; i++ {
-		num *= subjectNumber
-		num %= divisor
+		num = productMod(num, subjectNumber, divisor)
 	}
 
 	return num
